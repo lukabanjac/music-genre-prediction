99f4a589ea94dbf1e699dbdaa09a378e307452d9
Luka Banjac <banjacluka96@gmail.com>
2020-09-21 16:47:24 +0200

    dodano jos super zanrova; dodan resampling

--- genre_classifier.py
+++ genre_classifier.py
@@ -5,26 +5,34 @@ import csv
 import sys
 import json
 import time
+import pickle
 
-from sklearn.decomposition import PCA
-from sklearn.tree import DecisionTreeClassifier  
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors.classification import KNeighborsClassifier
-from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
-from sklearn.ensemble import GradientBoostingClassifier
+
+from imblearn.pipeline import make_pipeline as make_pipeline_imb
+from imblearn.over_sampling import SMOTE, ADASYN
+from imblearn.under_sampling import NearMiss
+
 from sklearn.feature_selection import SelectFromModel
-from sklearn.preprocessing import normalize, StandardScaler, MinMaxScaler
+from sklearn.preprocessing import normalize, MinMaxScaler
 from sklearn.model_selection import ShuffleSplit, learning_curve
 
+
+"""
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors.classification import KNeighborsClassifier
+from sklearn.svm import SVC
+"""
 
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 
-	
+
+#Pomocne funkcije za preprocesiranje
 def read_genres_(data):
 	end = len(data.index)
 	sum_others = 0
@@ -43,10 +51,12 @@ def read_genres_(data):
 				data['genres'].iloc[index] = assigned_genre
 
 			if assigned_genre == "other":
-				others.append(others_temp)
+				for item in others_temp:
+					others.append(item)
 				sum_others += 1
 		else:
-		    data["genres"].iloc[index] = "NO_ASSIGNED_GENRE"
+			data["genres"].iloc[index] = "NO_ASSIGNED_GENRE"
+
 		
 		sys.stdout.write("\033[F") #back to previous line 
 		sys.stdout.write("\033[K") #clear line 
@@ -57,16 +67,100 @@ def read_genres_(data):
 	data.drop(indexEmpty, inplace=True)
 	
 
-	#data = deal_with_others(data)
+	print("-"*30)
+	print("NUM OF OTHERS: ")
+	print(sum_others)
+	print("-"*30)
+	print(data)
+	print(others)
 
-	#print("-"*30)
-	#print("NUM OF OTHERS: ")
-	#print(sum_others)
-	#print("-"*30)
-	#print(data)
-	#print(others)
+	others_dict = {}
+	for other_genre in others:
+		if other_genre in others_dict:
+			others_dict[other_genre] += 1
+		else:
+			others_dict[other_genre] = 1
+
+	others_dict_s = pd.Series(others_dict, name="num_of_occ")
+	others_dict_s = others_dict_s.sort_values(ascending=False)
+	others_dict_s.to_csv("counted_others.csv")
 
 	data.to_pickle('data.pkl')
+	
+def assign_super_genre(genres: list):
+	 
+	super_genres_dict = {
+		"metal": ["metal", "grindcore", "metalcore", "nu-metalcore", "thrash", "power metal"],
+		"punk": ["punk", "anarcho-punk", "cowpunk", "oi"],
+		"blues": ["blues", "boogie-woogie", "boogie"],
+		"jazz": ["jazz", "swing", "bop", "ragtime", "dixieland", "schlager"],
+		"classical": ["orchestra", "orchestral", "classical", "baroque", "chamber", "renaissance", "piano", "romantic", "romanticism", "symphony", "violin", "opera"],
+		"latino": ["latino", "merengue", "mariachi", "reggaeton", "cumbia", "salsa", "cha-cha-cha", "tango", "bachata", "mexican", "samba", "merenge", "flamenco", "rumba", "cuban", "latin"],
+		"r&b/soul": ["r&b", "soul", "disco", "doo-wop", "funk", "motown", "rhythm and blues"],
+		"country": ["country", "bluegrass", "honky tonk", "honky tonk", "americana", "redneck"],
+		"reggae": ["reggae", "roots reggae", "dub", "lovers rock"],
+		"hip-hop/rap": ["rap", "hip hop", "bounce"],
+		"edm": ["breakbeat", "dance", "grime", "dubstep", "electronic", "eurodance", "eurobeat", "house", "edm", "dnb", "drum and bass", "jungle", "techno", "trance", "chillhop", "lo-fi"],
+		"folk": ["folk", "polka"],
+		"rock": ["rock", "post-grunge", "grunge"],
+		"pop": ["britpop", "pop"],
+		"adult standards": ["adult standards"],
+		"show tunes": ["show tunes", "broadway", "movie tunes", "hollywood", "disney"],
+		"worship": ["worship", "gospel", "ccm", "christian"],
+		"talk": ["comedy", "poetry", "reading"],
+	}
+
+	super_genres_dict_scores = {}
+
+	for super_genre in super_genres_dict:
+		super_genres_dict_scores[super_genre] = 0
+		for sub_genre in super_genres_dict[super_genre]:
+			for genre in genres:
+				if sub_genre in genre:
+					super_genres_dict_scores[super_genre] += 1
+
+	most_relevant_genre = max(super_genres_dict_scores, key=super_genres_dict_scores.get)
+
+	if super_genres_dict_scores[most_relevant_genre] != 0:
+		return most_relevant_genre, []
+	else:
+		return "other", genres
+
+def find_genre_by_artist(artist, genre_df):
+	"""
+	for index_artist, value_artist in genre_df.iterrows():
+			if genre_df.loc[index_artist, "artists"] == artist:
+				return data.loc[index_artist, "genres"]
+	"""
+	artist_index = genre_df[genre_df['artists'].str.contains(artist, regex=False)].index 
+	if genre_df.loc[artist_index, "genres"].size != 0:
+		return genre_df.loc[artist_index, "genres"].iloc[0]
+	else:
+		return "NO_ASSIGNED_GENRE"
+
+def add_generic_genres_to_data(data_w_generes, whole_data_wo_generes):
+	whole_data_w_genres = whole_data_wo_generes
+
+	end = len(whole_data_w_genres.index)
+	i = 1
+	for index_entry, value_entry in whole_data_w_genres.iterrows():
+		artists_str = whole_data_w_genres.loc[index_entry, "artists"]
+		artist_list = eval(artists_str)
+		whole_data_w_genres.loc[index_entry, "genres"] = find_genre_by_artist(artist_list[0], data_w_generes)
+		sys.stdout.write("\033[F") #back to previous line 
+		sys.stdout.write("\033[K") #clear line 
+		print((i/end)*100)
+		i += 1
+
+	indexEmpty = whole_data_w_genres[whole_data_w_genres["genres"] == "NO_ASSIGNED_GENRE"].index
+	whole_data_w_genres.drop(indexEmpty, inplace=True)
+
+	whole_data_w_genres.to_csv("data-genres.csv")
+
+	print("* Finished with adding genres pardner!")
+
+
+
 
 def remove_outliers(data: pd.DataFrame, threshold):
 		
@@ -126,78 +220,6 @@ def remove_outliers(data: pd.DataFrame, threshold):
 		print(data.size)
 	return data
 
-			
-
-def assign_super_genre(genres: list):
-	 
-	super_genres_dict = {
-		"metal": ["metal", "grindcore", "metalcore", "nu-metalcore", "thrash", "power metal"],
-		"punk": ["punk", "anarcho-punk", "cowpunk", "oi"],
-		"blues": ["blues", "boogie-woogie", "boogie"],
-		"jazz": ["jazz", "swing", "bop", "ragtime", "dixieland", "schlager"],
-		"classical": ["orchestra", "orchestral", "classical", "baroque", "chamber", "renaissance", "piano", "romantic", "romanticism", "symphony", "violin"],
-		"latino": ["latino", "merengue", "mariachi", "reggaeton", "cumbia", "salsa", "cha-cha-cha", "tango", "bachata", "mexican", "samba", "merenge", "flamenco", "rumba", "cuban", "latin"],
-		"r&b/soul": ["r&b", "soul", "disco", "doo-wop", "funk", "motown", "rhythm and blues"],
-		"country": ["country", "bluegrass", "honky tonk", "honky tonk", "americana", "redneck"],
-		"reggae": ["reggae", "roots reggae", "dub", "lovers rock"],
-		"hip-hop/rap": ["rap", "hip hop", "bounce"],
-		"edm": ["breakbeat", "dance", "grime", "dubstep", "electronic", "eurodance", "eurobeat", "house", "edm", "dnb", "drum and bass", "jungle", "techno", "trance", "chillhop", "lo-fi"],
-		"folk": ["folk", "polka"],
-		"rock": ["rock", "post-grunge", "grunge"],
-		"pop": ["britpop", "pop"],
-		"adult standards": ["adult standards"],
-		"talk": ["comedy", "poetry", "reading"],
-	}
-
-	super_genres_dict_scores = {}
-
-	for super_genre in super_genres_dict:
-		super_genres_dict_scores[super_genre] = 0
-		for sub_genre in super_genres_dict[super_genre]:
-			for genre in genres:
-				if sub_genre in genre:
-					super_genres_dict_scores[super_genre] += 1
-
-	most_relevant_genre = max(super_genres_dict_scores, key=super_genres_dict_scores.get)
-
-	if super_genres_dict_scores[most_relevant_genre] != 0:
-		return most_relevant_genre, []
-	else:
-		return "other", genres
-
-def find_genre_by_artist(artist, genre_df):
-	"""
-	for index_artist, value_artist in genre_df.iterrows():
-			if genre_df.loc[index_artist, "artists"] == artist:
-				return data.loc[index_artist, "genres"]
-	"""
-	artist_index = genre_df[genre_df['artists'].str.contains(artist, regex=False)].index 
-	if genre_df.loc[artist_index, "genres"].size != 0:
-		return genre_df.loc[artist_index, "genres"].iloc[0]
-	else:
-		return "NO_ASSIGNED_GENRE"
-
-def add_generic_genres_to_data(data_w_generes, whole_data_wo_generes):
-	whole_data_w_genres = whole_data_wo_generes
-
-	end = len(whole_data_w_genres.index)
-	i = 1
-	for index_entry, value_entry in whole_data_w_genres.iterrows():
-		artists_str = whole_data_w_genres.loc[index_entry, "artists"]
-		artist_list = eval(artists_str)
-		whole_data_w_genres.loc[index_entry, "genres"] = find_genre_by_artist(artist_list[0], data_w_generes)
-		sys.stdout.write("\033[F") #back to previous line 
-		sys.stdout.write("\033[K") #clear line 
-		print((i/end)*100)
-		i += 1
-
-	indexEmpty = whole_data_w_genres[whole_data_w_genres["genres"] == "NO_ASSIGNED_GENRE"].index
-	whole_data_w_genres.drop(indexEmpty, inplace=True)
-
-	whole_data_w_genres.to_csv("data_w_added_genres_1.csv")
-
-	print("* Finished with adding genres pardner!")
-
 def timer(f):
 	start = time.time()
 	res = f()
@@ -211,21 +233,42 @@ def build_model(X, Y):
 	model = timer(lambda: pipeline.fit(x_train, y_train))
 	return x_test, y_test, model
 
+def build_model_SMOTE(X_smote, Y_smote):
+	smote = SMOTE(random_state=4)
+	X, Y = smote.fit_resample(X_smote, Y_smote)
+
+	x_train, x_test, y_train, y_test = train_test_split(X, Y, stratify=Y, test_size=0.3)
+	smote_pipeline = make_pipeline(RandomForestClassifier(n_estimators=80, criterion='gini', max_features='auto', max_depth=40))
+	smote_model = timer(lambda: smote_pipeline.fit(x_train, y_train))
+	return x_test, y_test, smote_model
+
+
+def build_model_ADASYN(X_ada, Y_ada):
+	ada = ADASYN(random_state=4)
+	X, Y = ada.fit_resample(X_ada, Y_ada)
+
+	x_train, x_test, y_train, y_test = train_test_split(X, Y, stratify=Y, test_size=0.3)
+	ada_pipeline = make_pipeline(RandomForestClassifier(n_estimators=100, criterion='gini', max_features='auto', max_depth=50))
+	ada_model = timer(lambda: ada_pipeline.fit(x_train, y_train))
+	return x_test, y_test, ada_model
+
+def build_model_undersampling(X, Y):
+	x_train, x_test, y_train, y_test = train_test_split(X, Y, stratify=Y, test_size=0.3)
+	nearmiss_pipeline = make_pipeline_imb(NearMiss(), RandomForestClassifier(n_estimators=200, criterion='gini', max_features='auto', max_depth=100))
+	nearmiss_model = timer(lambda: nearmiss_pipeline.fit(x_train, y_train))
+	return x_test, y_test, nearmiss_model
+
+
+def print_results(true_value, prediction):
+	print("Accuracy: {}".format(accuracy_score(true_value, prediction)))
+
+
 if __name__ == "__main__":
 	pd.options.mode.chained_assignment = None
-	#read_genres()
-	#data__ = pd.read_csv("data_w_genres.csv")
-	#read_genres_(data__)
-	#data_w_genres = pd.read_pickle('data.pkl')
-	#whole_data_wo_genres = pd.read_csv('data.csv')
-	#data = add_generic_genres_to_data(data_w_genres, whole_data_wo_genres) 
-	data__ = pd.read_csv("data_w_added_genres_1.csv")
-	#read_genres_(dataa)
-	#data = pd.read_pickle("data.pkl")
+	data___ = pd.read_csv("data-genres.csv")
 
 
-
-	data = remove_outliers(data_, 3)
+	data = remove_outliers(data___, 3)
 
 
 	print("* Done readin' feller!")
@@ -234,194 +277,19 @@ if __name__ == "__main__":
 		["danceability", "energy", "speechiness", "acousticness", "instrumentalness",
 		 "valence", "tempo", "duration_ms", "liveness", "popularity"]]
 	Y = data['genres']
-	"""
-	#HEATMAP
-	fig = plt.figure(figsize=(13,6))
-	labels = X.columns.tolist()
-	ax1 = fig.add_subplot(111)
-	ax1.set_xticklabels(labels,rotation=90, fontsize=10)
-	ax1.set_yticklabels(labels,fontsize=10)
-	plt.imshow(X.corr(), cmap='bwr', interpolation='nearest')
-	plt.colorbar()
-	ax1.set_xticks(np.arange(len(labels)))
-	ax1.set_yticks(np.arange(len(labels)))
-	plt.show()
 
-	plt.scatter(X["energy"], X["danceability"])
-	plt.show()
-	"""
+
 	min_max = MinMaxScaler()
 	X_min_max = min_max.fit_transform(X)
-	x_test, y_test, model = build_model(X_min_max, Y)
 
-	plot_learning_curve()
+	
+	#x_test, y_test, model = build_model(X_min_max, Y)
+	x_test, y_test, model = build_model_SMOTE(X_min_max, Y)
+	#x_test, y_test, model = build_model_ADASYN(X_min_max, Y)
+	#x_test, y_test, model = build_model_undersampling(X_min_max, Y)
 
 	prediction = model.predict(x_test)
-	print("Accuracy: {}".format(accuracy_score(y_test, prediction)))
 
-	
-	prediction = model.predict(X)
-	print("Training set accuracy: {}".format(accuracy_score(Y, prediction)))
+	print_results(y_test, prediction)
 
 
-
-	"""
-	data_test = pd.read_csv("data.csv")
-	
-	X_data_test = data_test[
-		["danceability", "energy", "loudness", "speechiness", "acousticness", "instrumentalness",
-		 "valence", "tempo", "liveness", "popularity", "key", "mode"]]
-	data_test["genres"] = model.predict(X_data_test)
-
-	data_test.drop(["danceability", "energy", "loudness", "speechiness", "acousticness", "instrumentalness",
-		 "valence", "tempo", "liveness", "popularity", "key", "mode"], axis=1)
-	data_test.to_csv("predicted_data.csv")
-
-	"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	#print("* Startin' classification")
-	#data = pd.read_pickle("data.pkl")
-	#print(len(data['genres'].unique().tolist()))
-	
-	
-
-	#print(data[(data.genres == "rock")].mean())
-	#plt.scatter(data["energy"], data["loudness"])
-
-	#data_ = data[(data["genres"].str.contains("funk")) & (~data["genres"].str.contains("rock")) & (~data["genres"].str.contains("metal")) & (~data["genres"].str.contains("breaks"))]
-	#data_ = data[(data["genres"].str.contains("house")) ^ (data["genres"].str.contains("techno")) ^ (data["genres"].str.contains("trance")) ^ (data["genres"].str.contains("dubstep")) ^ (data["genres"].str.contains("drum and bass")) ^ (data["genres"].str.contains("dnb")) ^ (data["genres"].str.contains("hardstyle"))]
-	#data_ = data[data["genres"].str.contains("disco")]
-	#data_ = data[(data["speechiness"] > 0.4) & (data["liveness"] > 0.1)]
-
-	#print(data_)
-	#print(data_.describe())
-
-	#for i in range(1, 14):
-		#plt.hist(data_.iloc[:, i])
-		#plt.show()
-	
-
-	""" 
-  
-	
-	#x_train, y_train = dana_genre(data)
-	#x_train = normalize(x_train)
-	print("* Done splittin'")
-
-	
-  
-	#HEATMAP
-	fig = plt.figure(figsize=(13,6))
-	labels = x_train.columns.tolist()
-	ax1 = fig.add_subplot(111)
-	ax1.set_xticklabels(labels,rotation=90, fontsize=10)
-	ax1.set_yticklabels(labels,fontsize=10)
-	plt.imshow(x_train.corr(), cmap='bwr', interpolation='nearest')
-	plt.colorbar()
-	ax1.set_xticks(np.arange(len(labels)))
-	ax1.set_yticks(np.arange(len(labels)))
-	plt.show()
-	
-
-	"""
-
-  
-
-	"""
-	#REGRESSION
-	sel = SelectFromModel(LogisticRegression(penalty='l1', C=100))
-	sel.fit(x_train, y_train)
-	print(sel.get_support())
-
-	x_train = sel.transform(x_train)
-	x_test = sel.transform(x_test)
-
-
-	sc = StandardScaler()
-	x_train = sc.fit_transform(x_train)
-	x_test = sc.transform(x_test)
-
-
-	"""
-	
-
-	#PCA
-	#x_train, x_test = do_PCA(4, x_train, x_test)
-	#print("* Done PCA-in'")
-
-	#classifier =  SVC(kernel='linear',gamma=0.01, C=0.01)
-	#classifier = GradientBoostingClassifier(max_depth=3, n_estimators=700)
-	#classifier = RandomForestClassifier(n_estimators=200, criterion='gini', max_features='auto', max_depth=100)
-	#classifier = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=3), n_estimators=4)
-	#classifier = KNeighborsClassifier(n_neighbors=1)
-	
-	
-   
-	"""
-	classifier.fit(x_train, y_train)
-	y_pred = classifier.predict(x_test)
-	acc = accuracy_score(y_test, y_pred)
-	print(acc)
-	
-	#title = "Learning curve"
-	#cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-	#fig, axes = plt.subplots(3, 2, figsize=(10, 15))
-	#plot_learning_curve(classifier, title, x_train, y_train, axes=axes[:, 1], ylim=(0.7, 1.01), cv=cv, n_jobs=4)
-
-	#plt.show()
-
-
-	x_ext, other_ext = read_ext_data("data.csv")
-
-	x_ext = sc.transform(x_ext)
-
-	y_pred_ext = classifier.predict(x_ext)
-
-	other_ext.insert(loc=3, column="predicted_genres", value=y_pred_ext)
-
-	other_ext.to_csv("predicted_data.csv")
-	print(other_ext.nunique())
-
-	
-	"""
-	
